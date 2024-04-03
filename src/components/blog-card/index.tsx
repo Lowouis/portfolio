f744c495c8381a4e515a9416414192bfa817f504
Admin <louisguritapro@gmail.com>
2024-04-03 11:58:20 +0200

    fixes?

--- src/components/blog-card/index.tsx
+++ src/components/blog-card/index.tsx
@@ -188,7 +188,7 @@ const BlogCard = ({
                     skeleton({ widthCls: 'w-28', heightCls: 'h-8' })
                   ) : (
                     <span className="text-base-content opacity-70">
-                      Blogs
+                      My Articles
                     </span>
                   )}
                 </h5>
